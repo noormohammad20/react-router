223a5fe01055e463fd7aa34b9e3d4132f5533c12
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 21:21:43 +0600

    project setup

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>Hello Friends How Are You</h1>
+        </div>
+    )
+}
+
+export default Friends
