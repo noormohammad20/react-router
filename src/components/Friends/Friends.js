5486b443d89d75e89ae4a8b8041de58a43e18a89
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 22:13:34 +0600

    Load Users, display users, dynamic link, use Navigate

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Friend from '../Friend/Friend'
 
 const Friends = () => {
+    const [friends, setFriends] = useState([])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setFriends(data))
+    }, [])
     return (
         <div>
-            <h1>Hello Friends How Are You</h1>
+            <h1>Hello Friends How Are You!</h1>
+            <p><small>Parle Kichu Taka Dhar Dis...: {friends.length}</small></p>
+            {
+                friends.map(friend => <Friend key={friend.id} friend={friend}></Friend>)
+            }
         </div>
     )
 }
