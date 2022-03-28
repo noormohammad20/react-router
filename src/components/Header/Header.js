4d69e042a8f98eb033025e52bcfe3a66a39f1120
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 22:53:58 +0600

    et Header, Nav, Create Link, display active route

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CustomLink from '../CustomLink/CustomLink'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome To My Fancy Routing!!</h1>
+            <nav>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/friends'>Friends</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
+
+
+            </nav>
+        </div >
+    )
+}
+
+export default Header
