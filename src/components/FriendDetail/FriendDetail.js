6f589a4d312aef5468cc7932fb7fb6e6dd061fd7
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 23:41:02 +0600

    React route parameter and load data based on dynamic route

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const FriendDetail = () => {
+    const [friend, setFriend] = useState({})
+
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [])
     const { friendId } = useParams()
     return (
         <div>
             <h2>This Is Detail Of A Friend {friendId}</h2>
+            <h3>name:{friend.name}</h3>
+            <p>Email:{friend.email}</p>
+            <h4>Site:{friend.website}</h4>
+            <p><small>City:{friend.address?.city}</small></p>
+            <p><small>Lat:{friend.address?.geo?.lat}</small></p>
         </div>
     )
 }
